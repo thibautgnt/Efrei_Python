13a07832ae0055dadb909c1e7e70f0c41c11cc1d
thibautgnt <thibaut0genet@gmail.com>
2024-02-13 14:30:37 +0100

    Update lab8.py

--- module3/lab8.py
+++ module3/lab8.py
@@ -3,3 +3,12 @@ upgraded (pretty) the vowel eater coded in the last lab.
 You have to created wordWithoutVovels and assigned an empty string to it.
 Use concatenation operation to ask Python to combine selected letters into a longer string during subsequent loop turns, and assign it to the wordWithoutVovels variable.
 """
+
+userWord = input("Enter a word: ")
+userWord = userWord.upper()
+wordWithoutVovels = ""
+for letter in userWord:
+    if letter in "AEIOU":
+        continue
+    wordWithoutVovels += letter
+print(wordWithoutVovels)

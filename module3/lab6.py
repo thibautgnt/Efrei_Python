26e318de211056d8126eb319d3812996f18b2aa9
thibautgnt <thibaut0genet@gmail.com>
2024-02-13 14:29:36 +0100

    up

--- module3/lab6.py
+++ module3/lab6.py
@@ -1,3 +1,8 @@
 """
 Design a program that uses a while loop and continuously asks the user to enter a word (The question should be inside the loop) unless the user enters "chupacabra" as the secret exit word, in which case the message "You've left the loop." should be printed to the screen, and the loop should terminate.
 """
+
+word = input("Enter a word: ")
+while word != "chupacabra":
+    word = input("Enter a word: ")
+print("You've left the loop.")

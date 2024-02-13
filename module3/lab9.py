e0a6069e9b852fd95f8c5688df794e4824b2f24c
thibautgnt <thibaut0genet@gmail.com>
2024-02-13 14:31:05 +0100

    Update lab9.py

--- module3/lab9.py
+++ module3/lab9.py
@@ -2,3 +2,12 @@
 Your task is to write a program which reads the number of blocks the builders have, and outputs the height of the pyramid that can be built using these blocks.
 The height is measured by the number of fully completed layers - if the builders don't have a sufficient number of blocks and cannot complete the next layer, they finish their work immediately. Important : each lower layer contains one block more than the layer above.
 """
+
+blocks = int(input("Enter the number of blocks: "))
+height = 0
+in_layer = 1
+while in_layer <= blocks:
+    height += 1
+    blocks -= in_layer
+    in_layer += 1
+print("The height of the pyramid:", height)

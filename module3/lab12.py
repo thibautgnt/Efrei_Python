a8e31443bb765d4a9e52d96115fccca33a123603
thibautgnt <thibaut0genet@gmail.com>
2024-02-13 14:55:07 +0100

    Update lab12.py

--- module3/lab12.py
+++ module3/lab12.py
@@ -1,3 +1,13 @@
 """
 Your task is to write a program which removes all the number repetitions from the list “myList”. The goal is to have a list in which all the numbers appear not more than once. You can Use : for loop, intermediate list and the not in operator
 """
+
+myList = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+newList = []
+for i in myList:
+    if i not in newList:
+        newList.append(i)
+myList = newList
+print("The list with unique elements only:")
+
+print(myList)

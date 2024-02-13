f493edcbbdbb4310dd6eee2be67afc73e73ee8c6
thibautgnt <thibaut0genet@gmail.com>
2024-02-13 14:44:04 +0100

    Update lab10.py

--- module3/lab10.py
+++ module3/lab10.py
@@ -10,3 +10,14 @@ Write a program which reads c0 and executes the above steps as long as c0 remain
 
 We also want you to count the steps needed to achieve the goal. Your code should output all the intermediate values of c0, too (Use while loop).
 """
+
+c0 = int(input("Enter a number: "))
+steps = 0
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 = c0 // 2
+    else:
+        c0 = 3 * c0 + 1
+    print(c0)
+    steps += 1
+print("steps =", steps)
